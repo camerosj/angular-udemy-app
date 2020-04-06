e817171fc7d63013e4034ef12606a5e6b9f25472
camerosj <stuartcameron86@gmail.com>
2020-04-06 20:44:32 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PostCreateComponent } from './post-create/post-create.component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule],
   declarations: [ AppComponent, HelloComponent, PostCreateComponent ],
   bootstrap:    [ AppComponent ]
 })
