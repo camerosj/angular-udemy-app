69f245b74ae53e6275fdefbeff0efffb6b42af95
camerosj <stuartcameron86@gmail.com>
2020-04-07 21:11:38 +0100

    3 files modified

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 
 export class PostListComponent {
-
+/*   posts = [
+    {title: "First Post Title", content: "First post\'s content"},
+    {title: "Second Post Title", content: "Second post\'s content"},
+    {title: "Third Post Title", content: "Third post\'s content"}
+  ]; */
+  posts=[];
 }
