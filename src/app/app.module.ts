1bf703d1299c8186ca2c2348f80ad793d3efa751
camerosj <stuartcameron86@gmail.com>
2020-04-07 20:16:43 +0100

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { PostCreateComponent } from './post-create/post-create.component'
+import { PostCreateComponent } from './post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule],
-  declarations: [ AppComponent, HelloComponent, PostCreateComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule ],
+  declarations: [ AppComponent, HelloComponent, PostCreateComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
